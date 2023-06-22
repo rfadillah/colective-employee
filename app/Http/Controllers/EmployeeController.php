4c30791980e576b6fb964ed14f1d2802af10a5be
Rizki fadillah <rizki.fadillah.20@student.is.ittelkom-sby.ac.id>
2023-06-22 21:53:51 +0700

    view employee list

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $pageTitle = 'Employee List';
+        // RAW SQL QUERY
+        // $employees = DB::table('employees')
+        //     ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
+        //     ->select('employees.*', 'positions.name as position_name', 'employees.id as employee_id')
+        //     ->get();
+
+        $employees = Employee::all();
+
+        return view('Employee.index', [
+            'pageTitle' => $pageTitle,
+            'employees' => $employees
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
